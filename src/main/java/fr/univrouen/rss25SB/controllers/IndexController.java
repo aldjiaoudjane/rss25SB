339628e13fa42c699c47821aca0697b849e933cb
aldjiaoudjane <aldjia1999al@gmail.com>
2025-05-23 15:50:51 +0200

    resoudre le deuxieme conflit de route

--- src/main/java/fr/univrouen/rss25SB/controllers/IndexController.java
+++ src/main/java/fr/univrouen/rss25SB/controllers/IndexController.java
@@ -1,15 +1,22 @@
 package fr.univrouen.rss25SB.controllers;
+
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import fr.univrouen.rss25SB.model.Item;
+
 @RestController
 public class IndexController {
-@GetMapping("/")
-public String index() {
-return "Hello rss25SB !";
-}
-@RequestMapping(value = "/xml", produces = MediaType.APPLICATION_XML_VALUE)
-public @ResponseBody Item getXML() {
-    return new Item("12345678", "Test item", "2022-05-01T11:22:33");
-}
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello rss25SB !";
+    }
+
+    @RequestMapping(value = "/xml", produces = MediaType.APPLICATION_XML_VALUE)
+    public @ResponseBody Item getXML() {
+        return new Item("12345678", "Test item", "2022-05-01T11:22:33");
+    }
 }
--- src/main/java/fr/univrouen/rss25SB/controllers/PostController.java
+++ src/main/java/fr/univrouen/rss25SB/controllers/PostController.java
@@ -1,35 +1,28 @@
 package fr.univrouen.rss25SB.controllers;
 
 import fr.univrouen.rss25SB.model.TestRSS;
-import org.springframework.http.MediaType;
 import fr.univrouen.rss25SB.model.Item;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/post")
 public class PostController {
-	@RequestMapping(value = "/testpost", method = RequestMethod.POST, 
-			consumes = "application/xml")
-			public String postTest(@RequestBody String flux) {
-			return ("<result><response>Message reçu : </response>" 
-			+ flux + "</result>");
-			}
-	
-	 @PostMapping(value = "/postrss", produces = MediaType.APPLICATION_XML_VALUE)
-	    @ResponseBody
-	    public String postRSS() {
-	        TestRSS rss = new TestRSS();
-	        return rss.loadFileXML();
-	    }
-	 
-	 @RequestMapping(value = "/xml", produces = MediaType.APPLICATION_XML_VALUE)
-	 public @ResponseBody Item getXML() {
-	 Item it = new Item("12345678","Test item","2022-05-01T11:22:33");
-	 return it;
-	 }
-	
+
+    @PostMapping(value = "/testpost", consumes = "application/xml")
+    public String postTest(@RequestBody String flux) {
+        return "<result><response>Message reçu : </response>" + flux + "</result>";
+    }
+
+    @PostMapping(value = "/postrss", produces = MediaType.APPLICATION_XML_VALUE)
+    @ResponseBody
+    public String postRSS() {
+        TestRSS rss = new TestRSS();
+        return rss.loadFileXML();
+    }
+
+    @RequestMapping(value = "/xml", produces = MediaType.APPLICATION_XML_VALUE)
+    public @ResponseBody Item getXML() {
+        return new Item("12345678", "Test item", "2022-05-01T11:22:33");
+    }
 }
