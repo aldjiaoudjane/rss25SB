40836b552e9b7c3755dbc6652d0562f4a5caf44e
aldjiaoudjane <aldjia1999al@gmail.com>
2025-05-23 19:32:41 +0200

    modification de item apres le premier deploiement du tp 8

--- src/main/java/fr/univrouen/rss25SB/model/Item.java
+++ src/main/java/fr/univrouen/rss25SB/model/Item.java
@@ -4,20 +4,8 @@ import java.time.OffsetDateTime;
 import java.util.List;
 
 import fr.univrouen.rss25SB.adapters.OffsetDateTimeAdapter;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
-import jakarta.xml.bind.annotation.XmlAccessType;
-import jakarta.xml.bind.annotation.XmlAccessorType;
-import jakarta.xml.bind.annotation.XmlElement;
-import jakarta.xml.bind.annotation.XmlElements;
-import jakarta.xml.bind.annotation.XmlTransient;
+import jakarta.persistence.*;
+import jakarta.xml.bind.annotation.*;
 import jakarta.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 @Entity
@@ -29,17 +17,18 @@ public class Item {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     @XmlElement(namespace = "http://univ.fr/rss25", required = true)
     private String guid;
 
+    @Column(nullable = false)
     @XmlElement(namespace = "http://univ.fr/rss25", required = true)
     private String title;
 
-    // Relations JPA cachées à JAXB
     @XmlTransient
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id")
-    private List<Category> category;
+    private List<Category> categories;
 
     @XmlElement(namespace = "http://univ.fr/rss25")
     @XmlJavaTypeAdapter(OffsetDateTimeAdapter.class)
@@ -50,21 +39,23 @@ public class Item {
     private OffsetDateTime updated;
 
     @XmlTransient
-    @OneToOne(cascade = CascadeType.ALL)
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "image_id")
     private Image image;
 
     @XmlTransient
-    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     @JoinColumn(name = "content_id")
     private Content content;
 
     @XmlTransient
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id")
     private List<PersonType> authorsAndContributors;
 
-    
+    // Constructeur par défaut requis par JPA
+    public Item() {}
+
     // Getters / Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
@@ -75,8 +66,8 @@ public class Item {
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
 
-    public List<Category> getCategory() { return category; }
-    public void setCategory(List<Category> category) { this.category = category; }
+    public List<Category> getCategories() { return categories; }
+    public void setCategories(List<Category> categories) { this.categories = categories; }
 
     public OffsetDateTime getPublished() { return published; }
     public void setPublished(OffsetDateTime published) { this.published = published; }
@@ -91,5 +82,7 @@ public class Item {
     public void setContent(Content content) { this.content = content; }
 
     public List<PersonType> getAuthorsAndContributors() { return authorsAndContributors; }
-    public void setAuthorsAndContributors(List<PersonType> authorsAndContributors) { this.authorsAndContributors = authorsAndContributors; }
+    public void setAuthorsAndContributors(List<PersonType> authorsAndContributors) {
+        this.authorsAndContributors = authorsAndContributors;
+    }
 }
